75e520ca2d925db352e0477361b57135186b7cce
wjb321 <1453521207@qq.com>
2022-08-30 05:59:19 +0800

    contact management system UI

---
+++ c++/77.adress_book_management/77.adress_book_management/77.contactbook_management.cpp
@@ -0,0 +1,20 @@
+#include "iostream"
+using namespace std;
+#include "string"
+void showinterface()
+{
+	cout << "***** 1. add contact person *****" << endl;
+	cout << "***** 2. add contact person *****" << endl;
+	cout << "***** 3. add contact person *****" << endl;
+	cout << "***** 4. add contact person *****" << endl;
+	cout << "***** 5. add contact person *****" << endl;
+	cout << "***** 6. add contact person *****" << endl;
+	cout << "***** 0. add contact person *****" << endl;
+
+}
+int main()
+{   
+	showinterface();
+	system("pause");
+	return 0;
+}
