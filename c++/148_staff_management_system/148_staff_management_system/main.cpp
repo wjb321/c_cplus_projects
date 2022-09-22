c117ca411f749d4439dffe52b63908bcc1519f05
wjb321 <1453521207@qq.com>
2022-09-23 05:59:43 +0800

    add employee function

--- c++/148_staff_management_system/148_staff_management_system/main.cpp
+++ c++/148_staff_management_system/148_staff_management_system/main.cpp
@@ -54,3 +54,51 @@ int main()
 	system("pause");
 	return 0;
 }
+
+
+//#include<iostream>
+//using namespace std;
+//int quickSort(int a[], int, int);
+//int main()
+//{   
+//	int finalVal = 0;
+//	int array[] = { 34, 65, 12, 43, 67, 5, 78, 10, 3, 70 }, k;
+//	int len = sizeof(array) / sizeof(int);
+//	cout << "The orginal arrayare:" << endl;
+//	for (k = 0; k<len; k++)
+//		cout << array[k] << ",";
+//	cout << endl;
+//	finalVal = quickSort(array, 0, len - 1);
+//	cout << "The sorted arrayare:" << endl;
+//	for (k = 0; k<len; k++)
+//		cout <<array[k] << ",";
+//	cout << endl;
+//	cout << "gh:" <<  finalVal << ",";
+//	system("pause");
+//	return 0;   
+//}
+//
+//int quickSort(int s[], int l, int r)
+//{
+//	int medianVal = 0;
+//	medianVal = (int)(r + 1) / 2.0;
+//	if (l< r)
+//	{
+//		int i = l, j = r, x = s[l];
+//		while (i < j)
+//		{
+//			while (i < j && s[j] >= x) // 从右向左找第一个小于x的数
+//				j--;
+//			if (i < j)
+//				s[i++] = s[j];
+//			while (i < j && s[i]< x) // 从左向右找第一个大于等于x的数
+//				i++;
+//			if (i < j)
+//				s[j--] = s[i];
+//		}
+//		s[i] = x;
+//		quickSort(s, l, i - 1); // 递归调用
+//		quickSort(s, i + 1, r);
+//	}
+//	return s[medianVal];
+//}
--- c++/148_staff_management_system/148_staff_management_system/workermanager.cpp
+++ c++/148_staff_management_system/148_staff_management_system/workermanager.cpp
@@ -1,7 +1,16 @@
 #include "workermanager.h"
+#include "employee.h"
+#include "manager.h"
+#include "Boss.h"
+#include "worker.h"
 
 workermanager::workermanager()
 {
+	//initialize the attribution
+	this->m_EmpNum = 0;
+
+	this->m_EmpArray = NULL;
+
 
 }
 
@@ -19,6 +28,76 @@ void workermanager::show_menu()
 	cout << "**********************************************" << endl;
 }
 
+void workermanager::add_Emp()
+{
+	cout << "input the num of employees" << endl;
+	int addNum = 0;
+	cin >> addNum;
+	
+	if (addNum > 0)
+	{
+		int newSize = this->m_EmpNum + addNum;
+		// new some spce
+		Worker ** newSpace = new Worker *[newSize];
+
+		//copy the old data to the new space
+		if (this->m_EmpArray != NULL)
+		{
+			for (int i = 0; i < this->m_EmpNum; i++)
+			{
+				newSpace[i] = this->m_EmpArray[i];
+			}
+		}
+		//add new data
+		for (int i = 0; i < addNum; i++)
+		{
+			int id;
+			string name;
+			int dSelete;
+			cout << "please input " << i + 1 << "th" << "new employee number" << endl;
+			cin >> id;
+			cout << "please input " << i + 1 << "th" << "new employee name" << endl;
+			cin >> name;
+			cout << "please selete the position " << endl;
+			cout << "1. common worker" << endl;
+			cout << "2. manager" << endl;
+			cout << "3. boss" << endl;
+			cin >> dSelete;
+
+			Worker * worker = NULL;
+			switch (dSelete)
+			{
+			case 1:
+				worker = new Employee(id, name, 1);
+				break;
+			case 2:
+				worker = new Manager(id, name, 2);
+				break;
+			case 3:
+				worker = new Boss(id, name, 3);
+				break;
+			default:
+				break;
+			}
+			//create worker duty, store in the array
+			newSpace[this->m_EmpNum + i] = worker;
+		}
+		//release origin space
+		delete[] this->m_EmpArray;
+		
+		//change new space point address
+		this->m_EmpArray = newSpace;
+
+		//update worker numbers
+		this->m_EmpNum = newSize;
+
+		cout << "successfully add the " << addNum << "employees" << endl;
+	}
+	else
+	{
+		cout << "sorry, please input again" << endl;
+	}
+}
 
 void workermanager::Exitsystem()
 {
